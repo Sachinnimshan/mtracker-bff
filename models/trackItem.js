c4d5b665189f4cdb63ba7307d8e1449efd475924
Sachin Nimshan <sachinnimshan@gmail.com>
2023-01-31 07:59:36 +0530

    latest

--- models/trackItem.js
+++ models/trackItem.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const TrackitemSchema = new mongoose.Schema({
   title: { type: String, required: true, unique: true },
   amount: { type: Number, required: true },
-  type: { type: String, required: true },
+  type: { type: Boolean, required: true, default: true},
   date: { type: Date, required: true, default: new Date() },
   note: { type: String, default: "" },
 });
